5698919a9a67ea3aab0ca3a87378ef514d95abec
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-04-22 09:02:41 -0300

    Adicionando rotas e caminhos para as rotas

---
+++ routes/pedidos.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { route } = require('../app');
+const router = express.Router();
+
+//RETORNA TODOS OS PEDIDOS
+router.get('/', (req,res,next) => {
+    res.status(200).send({
+        message: 'Usando o GET dentro da rota de pedidos'
+    });
+});
+
+//RETORNA OS DADOS DE UM PEDIDO
+router.get('/:id_pedidos', (req, res, next) => {
+    const id = req.params.id_pedidos;
+    res.status(200).send({
+
+        message: 'Usando o GET de um pedidos exclusivo',
+        id: id
+    });
+});
+
+// ADICIONA UM PEDIDO
+router.post('/', (req,res,next) => {
+    res.status(201).send ({
+        mensagem: 'Usando o POST  dentro da rota de pedidos'
+    });
+});
+
+//ALTERA UM PEDIDO
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o PATCH dentro da rota pedidos'
+    });
+});
+
+//DELETA UM PEDIDO
+route.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o DELETE dentro da rota produtos'
+    })
+})
+
+
+module.exports = router;
---
+++ routes/produtos.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { route } = require('../app');
+const router = express.Router();
+
+//RETORNA TODOS OS PRODUTOS
+router.get('/', (req,res,next) => {
+    res.status(200).send({
+        message: 'Usando o GET dentro da rota de produtos'
+    });
+});
+
+//RETORNA OS DADOS DE UM PRODUTO
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    res.status(200).send({
+
+        message: 'Usando o GET de um produto exclusivo',
+        id: id
+    });
+});
+
+// ADICIONA UM PRODUTO
+router.post('/', (req,res,next) => {
+    res.status(201).send ({
+        mensagem: 'Usando o POST  dentro da rota de produtos'
+    });
+});
+
+//ALTERA UM PRODUTO
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o PATCH dentro da rota produtos'
+    });
+});
+
+//DELETA UM PRODUTO
+route.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando o DELETE dentro da rota produtos'
+    })
+})
+
+
+module.exports = router;
