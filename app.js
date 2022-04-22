22885c0f0e71cf8180ac6103cb7e5f5037fb05df
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-04-22 16:33:08 -0300

    reestruturando a app

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app = express();
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
+const app = express();
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos)
